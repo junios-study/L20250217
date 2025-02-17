bcf7bea809f308991448d0592e61b2d77ece2342
jhcsharp01 <makebaram@gmail.com>
2025-02-17 12:13:01 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L20250217
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            //engine.Load();
+
+            engine.Run();
+
+            //engine.Stop();
+        }
+    }
+}
