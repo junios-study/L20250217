4c3f21c76fcb9e98b4cbb633ee603cd8e4ffe4d2
jhcsharp01 <makebaram@gmail.com>
2025-02-17 15:36:04 +0900

    몬스터 AI 추가

--- Monster.cs
+++ Monster.cs
@@ -8,11 +8,35 @@ namespace L20250217
 {
     public class Monster : GameObject
     {
+        private Random rand = new Random();
+
         public Monster(int inX, int inY, char inShape)
         {
             X = inX;
             Y = inY;
             Shape = inShape;
+        }
+
+        public override void Update()
+        {
+            int Direction = rand.Next(0, 4);
+
+            if (Direction == 0)
+            {
+                Y--;
+            }
+            if (Direction == 1)
+            {
+                Y++;
+            }
+            if (Direction == 2)
+            {
+                X--;
+            }
+            if (Direction == 3)
+            {
+                X++;
+            }
         }
     }
 }
